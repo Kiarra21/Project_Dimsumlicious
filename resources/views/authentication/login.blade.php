ab9251967750b604367b3752b61f927c0c22c124
Kiarra Aja <177406907+Kiarra21@users.noreply.github.com>
2025-11-27 21:19:28 +0700

    ubah tampilan

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -207,12 +207,12 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                             </svg>
-                            Username
+                            Email
                         </label>
                         <div class="relative">
                             <input id="username" name="username" type="text" required
                                 class="appearance-none rounded-lg relative block w-full px-4 py-3 pl-12 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary focus:z-10 sm:text-sm transition-all duration-300 hover:border-secondary"
-                                placeholder="Masukkan username Anda" value="{{ old('username') }}">
+                                placeholder="Masukkan email Anda" value="{{ old('username') }}">
                             <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                 <svg class="h-5 w-5 text-gray-400" fill="none" stroke="currentColor"
                                     viewBox="0 0 24 24">
