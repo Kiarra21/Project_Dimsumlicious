61554ded0d885e551924d4b07e4d06ba439f30ac
Kiarra Aja <177406907+Kiarra21@users.noreply.github.com>
2025-11-27 23:48:33 +0700

    import storage

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Storage;
 
 class UserProfileController extends Controller
 {
